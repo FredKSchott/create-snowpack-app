78d41937dfef46529e9ae033aed9b27a934c6c69
Jovi De Croock <decroockjovi@gmail.com>
2020-05-18 17:10:34 +0200

    make @snowpack/plugin-babel compatible with v2

--- packages/plugin-babel/plugin.js
+++ packages/plugin-babel/plugin.js
@@ -3,11 +3,14 @@ const babel = require("@babel/core");
 module.exports = function plugin(config, options) {
   return {
     defaultBuildScript: "build:js,jsx,ts,tsx",
-    async build({ fileContents, filePath }) {
-      const result = await babel.transformAsync(fileContents, {
+    async build({ contents, filePath, fileContents }) {
+      const result = await babel.transformAsync(contents || fileContents, {
         filename: filePath,
         cwd: process.cwd(),
+        ast: false,
+        sourceMaps: false,
       });
+
       return { result: result.code };
     },
   };

c92bbe33210c5b82dfdd2b0590e0b190f0ef3aaf
34x <maks@34x.me>
2020-07-18 12:47:48 +0200

    Make use of svelte.config.js in app-scripts-svelte/jest.config.js

--- packages/app-scripts-svelte/jest.config.js
+++ packages/app-scripts-svelte/jest.config.js
@@ -9,13 +9,19 @@ const path = require("path");
 const setupTestsFile = true;
 
 module.exports = function () {
+
+  const userSvelteConfig = getUserSvelteConfig();
+
   return {
     testMatch: [
       "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
       "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}",
     ],
     transform: {
-      "^.+\\.svelte$": "jest-transform-svelte",
+      "^.+\\.svelte$": [
+        "jest-transform-svelte",
+        { preprocess: userSvelteConfig.preprocess },
+      ],
       "^.+\\.js$": path.resolve(__dirname, "jest/babelTransform.js"),
     },
     moduleFileExtensions: ["js", "svelte"],
@@ -26,3 +32,12 @@ module.exports = function () {
     setupFilesAfterEnv: setupTestsFile ? ["<rootDir>/jest.setup.js"] : [],
   };
 };
+
+function getUserSvelteConfig() {
+   const userSvelteConfigLoc = path.join(process.cwd(), "svelte.config.js");
+   if (fs.existsSync(userSvelteConfigLoc)) {
+     return require(userSvelteConfigLoc);
+   }
+
+   return {};
+}

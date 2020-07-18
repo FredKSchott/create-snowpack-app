af4a7a043bacd16157d57fa6a888b4a6497d2ae3
34x <maks@34x.me>
2020-07-18 13:54:08 +0200

    Update import App -> App.svelte to properly handle types in App.test.ts

--- templates/app-template-svelte-typescript/src/App.test.ts
+++ templates/app-template-svelte-typescript/src/App.test.ts
@@ -1,5 +1,5 @@
 import { render } from "@testing-library/svelte";
-import App from "./App";
+import App from "./App.svelte";
 
 test("renders learn svelte link", () => {
   const { getByText } = render(App);
